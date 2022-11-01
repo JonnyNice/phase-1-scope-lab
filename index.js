e1f1df508d03baf6935910143a7b3ee5d080d1bd
JonnyNice <116230053+JonnyNice@users.noreply.github.com>
2022-11-01 15:26:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-var customerName = "bob"
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'morgan';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob';
+}
